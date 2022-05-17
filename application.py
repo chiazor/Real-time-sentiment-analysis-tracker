81672c54b67939dfe4c3d5a6fca5611bb5ff8a26
Jennifer Kwentoh <kwentohchiazor@gmail.com>
2022-05-17 20:08:33 +0100

    update

--- application.py
+++ application.py
@@ -1,9 +1,15 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, request, jsonify
 from pymongo import MongoClient
 import joblib
-from sklearn.feature_extraction.text import TfidfTransformer
+from transformers import TextClassificationPipeline
+
+#from pymongo.errors import BulkWriteError
+
 
 application = Flask(__name__)
+application.config["MONGO_URI"] = "mongodb://localhost:27017/sentiment_db"
+mongodb_client = PyMongo(application)
+db = mongodb_client.db
 
 
 @application.route('/')
@@ -13,23 +19,28 @@ def home():
 
 @application.route('/predict', methods=['POST'])
 def predict():
-    vectorizer = open('vectorizer.pkl','rb')
-    cv = joblib.load(vectorizer)
-    _model = open('model.pkl','rb')
-    clf = joblib.load(_model)
+    tokenizer = open('tokenizer.pkl','rb')
+    tokenizer = joblib.load(tokenizer)
+    model = open('model.pkl','rb')
+    model = joblib.load(model)
+    pipe = TextClassificationPipeline(model=model, tokenizer=tokenizer)
 
     if request.method == 'POST':
         message = request.form['message']
         data = [message]
-        tf_transformer = TfidfTransformer()
-        vect = tf_transformer.fit_transform(data).toarray()
-        my_prediction = clf.predict(vect)
+        m_pipe = pipe(data)
+        my_prediction = m_pipe[0]['label']
 
-    return render_template('result.html', prediction=my_prediction)
+    return jsonify({'message': data, 'sentiment': m_pipe[0]['label'], 'score': m_pipe[0]['score']})
+
+# @application.route('/query')
+# def querydb():
+#     keywords = db.sentiment.find()
+#     return flask
 
 
 
 
 if __name__ == '__main__':
-    # app.run(debug=True)
+    #application.run(debug=True)
     application.run(host='0.0.0.0', port=80)
